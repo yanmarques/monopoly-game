221506bfef0d38fcabef72af8eebdb2c126d6684
Yan Marques <marques_yan@outlook.com>
2019-09-28 16:25:03 -0300

    Add nextInt helper method and add support for many generators

--- src/main/java/com/prj/commom/Dice.java
+++ src/main/java/com/prj/commom/Dice.java
@@ -3,12 +3,25 @@ package com.prj.commom;
 import java.util.Random;
 
 public class Dice {
+    private static Random randGen;
+
+    static {
+        setRandGenerator(new Random());
+    }
+
     public static int play() {
         return nextPositiveInt(6) + nextPositiveInt(6);
     }
 
+    public static int nextInt(int bound) {
+        return randGen.nextInt(bound);
+    }
+
     public static int nextPositiveInt(int bound) {
-        Random randGen = new Random();
-        return randGen.nextInt(bound) + 1;
+        return nextInt(bound) + 1;
+    }
+
+    public static void setRandGenerator(Random generator) {
+        randGen = generator;
     }
 }
