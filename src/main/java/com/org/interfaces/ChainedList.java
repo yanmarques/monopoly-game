eae4dba4ae0c75ce918703534b46df669312a0f1
Yan Marques <marques_yan@outlook.com>
2019-10-01 18:36:05 -0300

    Move data structures as a dependency module and remove unused files

--- src/main/java/com/org/interfaces/ChainedList.java
+++
@@ -1,9 +0,0 @@
-package com.org.interfaces;
-
-import com.org.Node;
-
-public interface ChainedList<T> extends List<T> {
-    void insert(int position, Node<T> Node) throws ArrayIndexOutOfBoundsException;
-    Node<T> remove(int position) throws ArrayIndexOutOfBoundsException;
-    Node<T> get(int position) throws ArrayIndexOutOfBoundsException;
-}
--- src/main/java/com/org/interfaces/StorageAccessor.java
+++
@@ -1,8 +0,0 @@
-package com.org.interfaces;
-
-import com.org.Node;
-
-public interface StorageAccessor<T> extends List<T> {
-    void insert(Node<T> node);
-    Node<T> remove();
-}
